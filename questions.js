b939cba3f5790069fe110b64063db1b0368b39e0
mazinabed <53720598+mazinabed@users.noreply.github.com>
2019-10-29 13:48:00 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,6 @@
+var questions = [
+    [ "Commonly used data types DO NOT include:", "strings", "alerts", "booleans", "B" ],
+	[ "The condition in an if / else statement is enclosed within ____.", "curly brackets", "square brackets", "parentheses", "C" ],
+	[ "JavaScript is ______ Language.?", "Programming", "Scripting", "Application", "A" ],
+	[ "Inside which HTML element do we put the JavaScript?", "JavaScript", "JS", "Script", "C" ]
+];
